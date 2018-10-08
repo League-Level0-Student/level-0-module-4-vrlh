97f4769d239ae4b85b8ed7aad5275eec1977aeed
11000783 <josephaguilar2022@gmail.com>
2018-10-07 17:26:08 -0700

    String Methods 1 and 2.

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -1,4 +1,7 @@
 package charAt_scanners_optionDialogs;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,24 +11,34 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String good = JOptionPane.showInputDialog(null, "Write something random");
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+System.out.println(good.charAt(2));		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(good.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
 
+for (int i=0; i<good.length(); i++ ) {
+	System.out.println(good.charAt(i));
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+for (int x=0; x<good.length(); x++) {
+		 if ('a' == (good.charAt(x))) {
+			 System.out.println("a is at index " + x);
+		 }
+		 
 	}
-}
+	}
+	
+	}
+
 
 
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -1,4 +1,7 @@
 package extra;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,18 +11,20 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
-		// 2. Print your String to the console in upper case.
-		
+String crazy = "HiHlkKJ";
+// 2. Print your String to the console in upper case.
+		System.out.println(crazy.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+System.out.println(crazy.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+		System.out.println(crazy.substring(0, 4));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(crazy.charAt(1));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(
+				crazy.substring(crazy.length()-3));
 	}
 }
